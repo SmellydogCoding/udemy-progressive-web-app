c1b67adca48b8fe525129a56a24e1342e181d7a8
SmellydogCoding <smellydogcoding@gmail.com>
2018-01-31 21:23:36 +0000

    start section 3 - service workers

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,55 +1,5 @@
-const title = document.querySelector('.title');
-const courseFeatureElements = document.querySelectorAll('.course-feature');
-const button = document.querySelector('button');
-
-navigator.serviceWorker.register('/sw.js');
-
-const animate = () => {
-  title.classList.remove('animate-in');
-  for (let i = 0; i < courseFeatureElements.length; i++) {
-    courseFeatureElements[i].classList.remove('animate-in');
-  }
-  button.classList.remove('animate-in');
-
-  setTimeout(() => {
-    title.classList.add('animate-in');
-  }, 1000);
-
-  setTimeout(() => {
-    courseFeatureElements[0].classList.add('animate-in');
-  }, 3000);
-
-  setTimeout(() => {
-    courseFeatureElements[1].classList.add('animate-in');
-  }, 4500);
-
-  setTimeout(() => {
-    courseFeatureElements[2].classList.add('animate-in');
-  }, 6000);
-
-  setTimeout(() => {
-    courseFeatureElements[3].classList.add('animate-in');
-  }, 7500);
-
-  setTimeout(() => {
-    courseFeatureElements[4].classList.add('animate-in');
-  }, 9000);
-
-  setTimeout(() => {
-    courseFeatureElements[5].classList.add('animate-in');
-  }, 10500);
-
-  setTimeout(() => {
-    courseFeatureElements[6].classList.add('animate-in');
-  }, 12000);
-
-  setTimeout(() => {
-    button.classList.add('animate-in');
-  }, 13500);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('registered');
+  });
 }
-
-animate();
-
-button.addEventListener('click', () => {
-  animate();
-});
--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,13 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('first-app')
-      .then(function(cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/css/app.css',
-          '/src/js/app.js'
-        ])
-      })
-  );
+self.addEventListener('install', (event) => {
+  console.log('install', event);
+  return self.clients.claim();
+});
+self.addEventListener('sctivate', (event) => {
+  console.log('activate', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+self.addEventListener('fetch', (event) => {
+  console.log('fetch', event);
+  event.respondWith(fetch(event.request));
 });
